e15e479d5c9ac33860c6d456dd105cfb7508815a
github username <github email address>
2018-12-23 22:46:23 -0500

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,44 +1,37 @@
+
+
 def roll_call_dwarves(array)
-  array.each_with_index do |name, index|
-    puts "#{index + 1}. #{name}"
-  end
+    array.each_with_index do |name, index|
+        puts "#{index + 1}. #{name}"
+    end
 end
+
 
 def summon_captain_planet(array)
-  array.map! {|name| name.capitalize }
-  return array.collect {|name| name + "!"}
+    # array.map {|name| name.capitalize + "!" }
+    array.map do |calls|
+        calls.capitalize + "!"
+    end
 end
+
 
 def long_planeteer_calls(array)
-  if array.any? {|word| word.length > 4}
-    return true
-  else
-    return false
-  end
+    if array.any? {|word| word.length > 4}
+        return true
+    else
+        return false
+    end
 end
+
 
 def find_the_cheese(array)
-  if array.include?("cheddar")
-    return "cheddar"
-  elsif array.include?("gouda")
-    return "gouda"
-  elsif array.include?("camembert")
-    return "camembert"
-  else
-    return nil
-  end
-  cheese_types = ["cheddar", "gouda", "camembert"]
+    if array.include?("cheddar")
+        return "cheddar"
+    elsif array.include?("gouda")
+        return "gouda"
+    elsif array.include?("camembert")
+        return "camembert"
+    else
+        return nil
+    end
 end
-
-# def find_the_cheese(array)
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-#   if array.include?(cheese_types)
-#     return "cheddar"
-#   elsif array.include?("gouda")
-#     return "gouda"
-#   elsif array.include?("camembert")
-#     return "camembert"
-#   else
-#     return nil
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-# end
